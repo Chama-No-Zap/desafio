9bb8091f665d3ddbaf0a136ebf1ce754e3be684c
Cardoso <lsc.cardosoo@gmail.com>
2020-08-17 23:04:17 -0300

    Updates

---
+++ precache-manifest.9b58761b24cb33dae4e5f6b19a2432e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d31fb22869895f6b3cb41501f91bbf34",
+    "url": "/desafio/index.html"
+  },
+  {
+    "revision": "ddf4bcdb83cf06992e6e",
+    "url": "/desafio/static/css/main.bf4fe683.chunk.css"
+  },
+  {
+    "revision": "81a58d2b0d6e2bb59749",
+    "url": "/desafio/static/js/2.bf697446.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/desafio/static/js/2.bf697446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf4bcdb83cf06992e6e",
+    "url": "/desafio/static/js/main.f59cf3da.chunk.js"
+  },
+  {
+    "revision": "6791c022fd2ccb9a7e4d",
+    "url": "/desafio/static/js/runtime-main.b36aee4a.js"
+  },
+  {
+    "revision": "fee025d75da5e9fbc7b199804f6c5882",
+    "url": "/desafio/static/media/A_Minha_Galeria.fee025d7.png"
+  }
+]);
